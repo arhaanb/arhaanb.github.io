5b755fd61f0c07e64e8aa67f7cfa4e962e348cb1
arhaanb <arhaanb@gmail.com>
2019-11-24 22:53:45 +0530

    terrestria

---
+++ cosmo/terrestria/main.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');
+       
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+      
+    }
+});
